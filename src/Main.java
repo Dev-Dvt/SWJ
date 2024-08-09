8dae8bcee3fab257cbb6fda2f8ea8801add21de7
DevDvt <devangthakar02@gmail.com>
2024-08-09 10:14:57 +0530

    login path added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -5,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","D:\\Setup Selenium\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-        driver.get("https://portfolio-tawny-beta-76.vercel.app/");
+        driver.get("http://indiaserver.sypramtechnology.com:5010/");
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div[6]/div/button")).click();
+//        driver.findElement(By.xpath("//*[@id=\\\"content\\\"]/div[1]/div[6]/div/button")).click();
     }
 }
